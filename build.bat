c0afb6657eb092be04a3fd2546f261d18b174f09
anistarafdar <atmew2@gmail.com>
2021-12-15 19:00:59 -0500

    fixed build error wit yuv442's code

--- build.bat
+++ build.bat
@@ -2,6 +2,8 @@ cd gba
 make clean
 make
 cd ..
+mkdir data
+@REM https://github.com/yuv422/gba-link-cable-dumper/commit/ab7d43aaaebb978879ca6410820752a43023d1a6
 mv -f gba/gba_mb.gba data/gba_mb.gba
 make -f Makefile.gc clean
 make -f Makefile.gc
